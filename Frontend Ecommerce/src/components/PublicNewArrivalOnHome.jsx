285ab8baac8783154db3292fb86c515cc43f64a6
Syed Jawad Ali <syedjawadali92@gmail.com>
2026-08-30 11:24:37 +0500

    Changes

--- Frontend Ecommerce/src/components/PublicNewArrivalOnHome.jsx
+++ Frontend Ecommerce/src/components/PublicNewArrivalOnHome.jsx
@@ -241,7 +241,7 @@ const NewArrivalOnHome = () => {
                 </div>
 
                 <div 
-                    className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8"
+                    className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 sm:gap-2 mx-4 sm:mx-0"
                     ref={productsRef}
                 >
                     {loading ? (
