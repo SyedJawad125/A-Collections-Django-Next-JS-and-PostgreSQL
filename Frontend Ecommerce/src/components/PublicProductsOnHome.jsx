795bed2eb692a4a6797b96551de0cdb2d6bd7723
Syed Jawad Ali <syedjawadali92@gmail.com>
2026-08-30 11:24:48 +0500

    Changes

--- Frontend Ecommerce/src/components/PublicProductsOnHome.jsx
+++ Frontend Ecommerce/src/components/PublicProductsOnHome.jsx
@@ -261,7 +261,7 @@ const PublicProductsOnHome = () => {
 
                 {/* Products Grid */}
                 <div 
-                    className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 min-h-[400px]" 
+                    className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 sm:gap-2 mx-4 sm:mx-0" 
                     ref={productsRef}
                 >
                     {loading ? (
